33c0d6a02abff4729a759f13971a5bec94e4e9e0
Maxwell Swadling <maxwellswadling@gmail.com>
2022-05-12 17:54:43 +1200

    Initial Commit

---
+++ EasyPass/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EasyPass
+//
+//  Created by Maxwell on 12/05/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
