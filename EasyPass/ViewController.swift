91acec233e7f6acda25266b163b3bc02085cebf1
Maxwell Swadling <maxwellswadling@gmail.com>
2022-11-09 21:24:38 +1300

    made the whole app in one go

--- EasyPass/ViewController.swift
+++ EasyPass/ViewController.swift
@@ -7,20 +7,48 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextFieldDelegate {
 
+    var item: KeychainItem!
+    @IBOutlet weak var showPassword: NSTextField!
+    @IBOutlet weak var passwordField: NSSecureTextField!
+    @IBOutlet weak var showButton: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        item = KeychainItem(service: "easypass", account: "default")
+        passwordField.delegate = self
+        passwordField.stringValue = (try? item.readItem()) ?? ""
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        DispatchQueue.main.async {
+            self.view.window?.makeFirstResponder(nil)
         }
     }
 
-
+    @IBAction func pressCopy(_ sender: Any) {
+        let password = try! item.readItem()
+        let pb = NSPasteboard.general
+        pb.clearContents()
+        pb.setString(password, forType: .string)
+    }
+    
+    func controlTextDidChange(_ obj: Notification) {
+        try! item.saveItem(passwordField.stringValue)
+        if showButton.state == .on {
+            showPassword.stringValue = passwordField.stringValue
+        }
+    }
+    
+    @IBAction func showPass(_ sender: Any) {
+        if showButton.state == .on {
+            showPassword.stringValue = try! item.readItem()
+        } else {
+            showPassword.stringValue = ""
+        }
+    }
+    
+    
 }
-
